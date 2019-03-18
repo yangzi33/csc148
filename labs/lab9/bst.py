a0b3ff024187d09a62ef391d9a39ca2cce66fecc
yangzi33 <ziyue.yang@mail.utoronto.ca>
2019-03-18 17:26:13 -0400

    Update bst.py

--- labs/lab9/bst.py
+++ labs/lab9/bst.py
@@ -358,6 +358,26 @@ class BinarySearchTree:
         # TODO: implement this method!
         pass
 
+    def closest(self, item: int) -> Optional[int]:
+        if self.is_empty():
+            return None
+        elif item == self._root:
+            return self._root
+        elif item < self._root:
+            left_closest = self._left.closest(item)
+            if left_closest is not None and \
+                    (item - left_closest) <= self._root - item:
+                return left_closest
+            else:
+                return self._root
+        else:
+            right_closest = self._right.closest(item)
+            if right_closest is not None and \
+                    (right_closest - item) < item - self._root:
+                return right_closest
+            else:
+                return self._root
+
 
 if __name__ == '__main__':
     import doctest
